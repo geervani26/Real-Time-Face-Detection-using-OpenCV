cb1c7019d24b65aec4938b7915a36aabfce56932
Geervani <154957644+geervani26@users.noreply.github.com>
2025-09-17 08:39:28 +0530

    Add files via upload

---
+++ face.py
@@ -0,0 +1,19 @@
+import cv2
+haar_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+cam=cv2.VideoCapture(0)
+while True:
+    _, img=cam.read()
+    grayImg=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces=haar_cascade.detectMultiScale(grayImg, 1.3, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    cv2.imshow("FaceDetection", img)
+    key=cv2.waitKey(10)
+    print(key)
+    if key==27:
+        break
+cam.release()
+cv2.destroyAllWindows()
+
+        
+    
